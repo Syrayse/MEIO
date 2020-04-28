4631faae3487be62b1c198d1abc00d5d305ddf66
syrayse <ruipedronetoreis12@gmail.com>
2020-04-28 16:19:13 +0100

    adicionada funcao para construcao de matriz de transicao

--- src/tp1.m
+++ src/tp1.m
@@ -1,11 +1,12 @@
-# Ficheiro que contem o codigo necessario
-
-
+# Dados Relativos a filial 1.
 pedidos1 =  [0.0356,0.0904,0.1380,0.1400,0.1224,0.1292,0.0952,0.0820,0.0560,0.0496,0.0324,0.0216, 0.0076];
 encomendas1 = [0.0448,0.1632,0.2220,0.2092,0.1620,0.1056,0.0556,0.0236,0.0100,0.0036,0.0000,0.0000,0.0004];
+
+# Dados Relativos a filial 2.
 pedidos2  = [0.0612,0.1204,0.1476,0.1228,0.1080,0.1100,0.0788,0.0776,0.0576,0.0516,0.0328,0.0236,0.0080];
 encomendas2 = [0.0192,0.0848,0.1540,0.1956,0.2040,0.1528,0.0884,0.0556,0.0284,0.0100,0.0040,0.0024,0.0008];
 
+# Calcula probabilidade associada a passar do estado i para j, sem transbordo.
 function val = p(i,j,C,pedidos,encomendas)
   val = 0;
   
@@ -18,6 +19,7 @@ function val = p(i,j,C,pedidos,encomendas)
   endfor
 endfunction
 
+# Calcula probabilidade de acontecer transbordo partindo de i.
 function val = phi(i,C,pedidos,encomendas)
   val = 0;
 
@@ -27,6 +29,20 @@ function val = phi(i,C,pedidos,encomendas)
     endfor
   endfor
 endfunction
+
+function val = buildTransitionMatrix(C,pedidos,encomendas)
+  val = zeros(C,C);
+  
+  for i = 0:C
+    for j = 0:C
+      val(i + 1,j + 1) = p(i,j,C,pedidos,encomendas);
+    endfor
+    
+    val(i + 1,C + 1) += phi(i,C,pedidos,encomendas);
+    
+  endfor
+endfunction
+
 
 function val = total(i,C,pedidos, encomendas)
   val = phi(i,C,pedidos,encomendas);
